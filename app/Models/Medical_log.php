73fce2997775e7ce0f298ec0dbfcd103bd2b3ba2
sirena <sirenalism@gmail.com>
2022-05-13 00:08:00 +0300

    =ML_Dental

--- app/Models/Medical_log.php
+++ app/Models/Medical_log.php
@@ -12,4 +12,8 @@ class Medical_log extends Model
     public function patient(){
         return $this->belongsTo(Patient::class,'id');
     }
+
+    public function ml_dental_clinic(){
+        return $this->hasOne(ML_Dental_Clinic::class,'id','id');
+    }
 }
