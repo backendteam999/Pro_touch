8e33b21fc1390ef0de0fa3af4a9b8e2119c3d571
sirena <sirenalism@gmail.com>
2022-05-12 23:32:39 +0300

    v

---
+++ app/Models/ML_Dermatology_Clinic.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ML_Dermatology_Clinic extends Model
+{
+    use HasFactory;
+}
---
+++ app/Models/ML_Nutrition_Clinic.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ML_Nutrition_Clinic extends Model
+{
+    use HasFactory;
+}
