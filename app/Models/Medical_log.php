620985bc8ac113986b4ab48bd552ab9c7c11dc31
fa171di <fa1791di@gmail.com>
2022-05-13 01:10:13 +0300

    j

--- app/Models/Medical_log.php
+++ app/Models/Medical_log.php
@@ -13,10 +13,13 @@ class Medical_log extends Model
         return $this->belongsTo(Patient::class,'id');
     }
     public function ML_Nutrition_Clinic(){
-        return $this->hasMany(ML_Nutrition_Clinic::class,'medical_log_id','id');
+        return $this->hasOne(ML_Nutrition_Clinic::class,'medical_log_id','id');
     }
     public function ML_Dermatology_laser_clinic(){
         return $this->hasOne(ML_Dermatology_laser_clinic::class,'medical_log_id','id');
+    }
+    public function ML_Dental_clinic(){
+        return $this->hasOne(ML_Dental_Clinic::class,'medical_log_id','id');
     }
 
 }
